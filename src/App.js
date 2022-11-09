67ceb11a006f09bc9a6d98a724ce88c9d8f108b9
Ibimina Hart <ibimina.c.hart@gmail.com>
2022-11-09 11:21:24 +0100

    Update App.js
    Docs: removed code comments

--- src/App.js
+++ src/App.js
@@ -3,28 +3,6 @@ import Header from "./Header";
 import Main from "./Main";
 
 class App extends React.Component {
-  // state = {  }
-  countriesPopulation = () => {
-    const tenHighestPopulation = [
-      { country: "World", population: 7693165599 },
-      { country: "China", population: 1377422166 },
-      { country: "India", population: 1295210000 },
-      { country: "USA", population: 323947000 },
-      { country: "Indonesia", population: 258705000 },
-      { country: "Brazil", population: 206135893 },
-      { country: "Pakistan", population: 194125062 },
-      { country: "Nigeria", population: 186988000 },
-      { country: "Bangladesh", population: 161006790 },
-      { country: "Russian", population: 146599183 },
-      { country: "Japan", population: 126960000 },
-    ];
-    // const {country,population} =
-    const val = tenHighestPopulation.map((list) => (
-      <li key={list.country}> {list.country} </li>
-    ));
-    return val;
-  };
-
   render() {
     const tenHighestPopulation = [
       { country: "World", population: 7693165599 },
@@ -39,21 +17,12 @@ class App extends React.Component {
       { country: "Russian", population: 146599183 },
       { country: "Japan", population: 126960000 },
     ];
-    // const population = tenHighestPopulation.map((list) => (
-    //   <li key={list.population}> {list.population} </li>
-    // ));
-    //  const country = tenHighestPopulation.map((list) => (
-    //    <li key={list.country}> {list.country} </li>
-    //  ));
-    //  let maxPo = tenHighestPopulation[0].population
-    //  const wrap = tenHighestPopulation.map((list)=> <li key={list.country} style={{maxWidth: (Number(list.population)/ Number(maxPo) ) *100 +"%"      }}> </li>)
-    const head = "30days of React";
+   const head = "30days of React";
     const subtitle = "World Population";
     const title = "Ten Most Populated Countries";
     return (
       <div className="wrapper">
         <Header head={head} subtitle={subtitle} title={title} />
-        {/* <Main  country={country} population={population} showLength={wrap} tenHighestPopulation={tenHighestPopulation}/> */}
         <Main tenHighestPopulation={tenHighestPopulation} />
       </div>
     );
